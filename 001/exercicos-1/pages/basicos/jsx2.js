b573b96f656460a5292edca66f43927115856e9b
codingscode <heronnestg@hotmail.com>
2022-01-16 22:23:13 -0300

    parte 7

--- 001/exercicos-1/pages/basicos/jsx2.js
+++ 001/exercicos-1/pages/basicos/jsx2.js
@@ -2,7 +2,23 @@
 
 export default function jsxDiferente2() {
 
-    const retorno = <div><h1>JSX #02</h1></div>
+    const array = [
+        <li>produto 1</li>,
+        <li>produto 2</li>,
+        <li>produto 3</li>,
+        <li>produto 4</li>,
+        <li>produto 5</li>
+    ]
+
+    const retorno = (
+       <div>
+           <h1>JSX #02</h1>
+           <div>
+              {array.map((cada, i) => (<div key={i}>{cada}</div>))}
+
+           </div>
+       </div>
+    )
 
     return retorno
  
