fb6850e71f95962e82af45d287a0ea9ac80df2b8
codingscode <heronnestg@hotmail.com>
2022-01-16 22:37:50 -0300

    parte 8

---
+++ 001/exercicos-1/pages/basicos/jsx3.jsx
@@ -0,0 +1,17 @@
+
+
+export default function jsxdif3() {
+
+    
+
+    const retorno = (
+       <div>
+           <h1>JSX #03</h1>
+           
+           
+       </div>
+    )
+
+    return retorno
+ 
+}
