3d71ebe8c543dc2333058b9c922fd85744ffdd17
codingscode <heronnestg@hotmail.com>
2022-01-18 20:34:30 -0300

    parte 9

---
+++ 001/exercicos-1/pages/basicos/jsx4.jsx
@@ -0,0 +1,17 @@
+
+
+export default function jsxdif4() {
+    const subtitulo = 'estou no subtitulo'
+    
+
+    return (
+       <div>
+          <h1>Integração JS e JSX</h1>
+          <h2>{subtitulo}</h2>
+          <h3>{3*7}</h3>
+          <h3>{Math.max(61, 18)}</h3>
+       </div>
+    )
+ 
+}
+
