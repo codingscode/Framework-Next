283db8c1b7944dd881fd56bfd2f329c0036f5468
codingscode <heronnestg@hotmail.com>
2022-01-18 20:41:38 -0300

    parte 10

--- 001/exercicos-1/pages/basicos/jsx4.jsx
+++ 001/exercicos-1/pages/basicos/jsx4.jsx
@@ -3,6 +3,7 @@
 export default function jsxdif4() {
     const subtitulo = 'estou no subtitulo'
     
+    const trechotag = <h2>outro h2</h2>
 
     return (
        <div>
@@ -10,8 +11,16 @@ export default function jsxdif4() {
           <h2>{subtitulo}</h2>
           <h3>{3*7}</h3>
           <h3>{Math.max(61, 18)}</h3>
+          {trechotag}
+          <h3>{ehpositivo(7)}</h3>
        </div>
     )
  
 }
 
+function ehpositivo(valor) {
+   if (valor >= 0) {
+      return 'sim'
+   }
+   return 'não'
+}
