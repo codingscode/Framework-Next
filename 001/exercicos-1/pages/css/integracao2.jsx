2ded9d4faf4f4bc681631204e35495c6020b2d93
codingscode <heronnestg@hotmail.com>
2022-01-23 23:00:11 -0300

    parte 24

--- 001/exercicos-1/pages/css/integracao2.jsx
+++ 001/exercicos-1/pages/css/integracao2.jsx
@@ -6,7 +6,7 @@ import estilos from './integracao2.module.css'
 export default function integracao2() {
 
     return (
-       <div>
+       <div id={estilos.integracao2} >
            <div className={estilos.vermelha}>Texto #01</div>
            <div className={estilos.azul}>Texto #02</div>
            <div className={estilos.branca}>Texto #03</div>
