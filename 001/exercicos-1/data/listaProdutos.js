89facc8d35419c89c71578e62ffc6fd5d8bc40c2
codingscode <heronnestg@hotmail.com>
2022-01-29 21:50:43 -0300

    parte 36

---
+++ 001/exercicos-1/data/listaProdutos.js
@@ -0,0 +1,17 @@
+import Produto from '../model/produto'
+
+
+
+export default [
+   new Produto(1, 'iPad', 6200),
+   new Produto(2, 'Galaxy', 1100),
+   new Produto(3, 'Computador', 5400),
+   new Produto(4, 'iMac', 17000),
+
+]
+
+
+
+
+
+
