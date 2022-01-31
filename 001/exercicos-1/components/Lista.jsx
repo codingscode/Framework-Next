9f29b333002cab460096d59a872d0cb25ae16580
codingscode <heronnestg@hotmail.com>
2022-01-30 22:41:31 -0300

    parte 43

--- 001/exercicos-1/components/Lista.jsx
+++ 001/exercicos-1/components/Lista.jsx
@@ -6,7 +6,7 @@ export default function Lista(props) {
    return (
       <div>
          <h1>Lista de algo</h1>
-         <ul>
+         <ul style={{ listStyle: 'none', padding: 0 }} >
             {props.children}
          </ul>
 
