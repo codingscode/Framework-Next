a7a79ca8275fac36e0515497de30030ba1738030
codingscode <heronnestg@hotmail.com>
2022-01-30 22:03:45 -0300

    parte 39

--- 001/exercicos-1/components/SomentePar.jsx
+++ 001/exercicos-1/components/SomentePar.jsx
@@ -5,15 +5,12 @@ export default function SomentePar(props) {
    const numeroPar = props.numero % 2 === 0
 
    return (
-      numeroPar ? <span>{props.numero} | </span> : null
+      <div>
+         { numeroPar ? <span>{props.numero} | </span> : null }
+      </div>
+      
    )
 
-   /*    if (props.numero % 2 === 0) {
-      return <span>{props.numero} | </span>
-   }
-   else {
-      return null
-   } */
 
 }
 
