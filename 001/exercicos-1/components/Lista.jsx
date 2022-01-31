e971f7f9d1a8ecfa5f503697058cf09c3eb917c0
codingscode <heronnestg@hotmail.com>
2022-01-30 22:30:13 -0300

    parte 41

--- 001/exercicos-1/components/Lista.jsx
+++ 001/exercicos-1/components/Lista.jsx
@@ -5,9 +5,7 @@ export default function Lista(props) {
 
    return (
       <ul>
-         <li>elemento #01</li>
-         <li>elemento #02</li>
-         <li>elemento #03</li>
+         {props.children}
       </ul>
    )
 }
