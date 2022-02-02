1f2b3a8e1ec162c1d2d29b877c6cb7b43d14b7b6
codingscode <heronnestg@hotmail.com>
2022-02-01 21:54:42 -0300

    parte 45

---
+++ 001/exercicos-1/components/direta/Pai.jsx
@@ -0,0 +1,24 @@
+import Filho from './Filho'
+
+
+export default function Pai() {
+
+   return (
+      <div>
+         <Filho nome="Silas" familia="Teixeira" />
+         <Filho nome="Paulo" familia="Silveira" />
+         <Filho nome="Simon" familia="Gomes" />
+         <Filho nome="Denis" familia="Lima" />
+      </div>
+   )
+}
+ 
+
+
+
+
+
+
+
+
+
---
+++ 001/exercicos-1/pages/comunicacao/direta.jsx
@@ -0,0 +1,13 @@
+import Pai from '../../components/direta/Pai'
+
+
+
+export default function direta() {
+
+   return (
+      <div>
+         <Pai />
+      </div>
+   )
+}
+
