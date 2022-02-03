493fc9a007829d0cc3024355549e4d1ecf89e31f
codingscode <heronnestg@hotmail.com>
2022-02-02 21:10:37 -0300

    parte 48

--- 001/exercicos-1/pages/basicos/botao.jsx
+++ 001/exercicos-1/pages/basicos/botao.jsx
@@ -13,13 +13,23 @@ export default function botao() {
       console.log('evento:')
       console.log(evento)
    }
+   
+   function acao5(evento) {
+      console.log('evento:')
+      console.log(evento)
+   }
 
    return (
+       
       <div>
          <button onClick={acao1} >Click #01</button>
          <button onClick={acao2} >Click #02</button>
          <button onClick={ () => console.log('acao3') } >Click #03</button>
          <button onClick={acao4} >Click #04</button>
+         <button onClick={ e => acao5(e.altKey)} >Click #05</button>
+         <div>
+            <input type="text" onChange={ (e) => console.log(e.target.value) }  />
+         </div>
       </div>
    )
 }
