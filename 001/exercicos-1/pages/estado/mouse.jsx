7b2723e3991f781f7cf6a093347a11733bccc519
codingscode <heronnestg@hotmail.com>
2022-02-04 22:02:33 -0300

    parte 53

--- 001/exercicos-1/pages/estado/mouse.jsx
+++ 001/exercicos-1/pages/estado/mouse.jsx
@@ -3,9 +3,16 @@
 
 export default function mouse() {
 
+   const estilos = { display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: "#222", color: "#fff", height: '100vh' } 
+
+   function quandoMove(ev) {
+      console.log(ev)
+   }
+
    return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: "#222", color: "#fff", height: '100vh' }} >
-         mouse
+      <div style={ estilos } onMouseMove={quandoMove} >
+         <span>Eixo X: </span>
+         <span>Eixo Y: </span>
       </div>
    )
 }
