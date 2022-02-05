a7a2bb947f1b10b050a46b04d047d0fbd5447e66
codingscode <heronnestg@hotmail.com>
2022-02-04 21:54:57 -0300

    parte 52

---
+++ 001/exercicos-1/pages/estado/mouse.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function mouse() {
+
+   return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: "#222", color: "#fff", height: '100vh' }} >
+         mouse
+      </div>
+   )
+}
+
