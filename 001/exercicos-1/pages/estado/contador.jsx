49ca836e96c555c760cac93dad4cf90fe2636bba
codingscode <heronnestg@hotmail.com>
2022-02-06 21:55:31 -0300

    parte 59

--- 001/exercicos-1/pages/estado/contador.jsx
+++ 001/exercicos-1/pages/estado/contador.jsx
@@ -5,14 +5,10 @@ import { useState } from 'react'
 export default function contador() {
    const [ numero, setNumero ] = useState(0)
 
-   const mais = () => {
-      setNumero(numero + 1)
-   }
-
-   const menos = () => {
-      setNumero(numero - 1)
-   }
-
+   const mais = () => setNumero(numero + 1)
+   
+   const menos = () => setNumero(numero - 1)
+   
 
    return (
       <div>
