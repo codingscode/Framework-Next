2548876a4467ed920f25d7d144412ab40252f032
codingscode <heronnestg@hotmail.com>
2022-02-12 09:01:12 -0300

    parte 71

--- 001/exercicos-1/components/Contador.jsx
+++ 001/exercicos-1/components/Contador.jsx
@@ -4,11 +4,17 @@ import { Component } from 'react'
 
 export default class Contador extends Component {
 
+   state = {
+      numero: 7
+   }
+   
+
    render() {
       return (
          <div>
             <h1>Contador (usando classe)</h1>
             <h2>{this.props.valorInicial}</h2>
+            <h2>{this.state.numero}</h2>
          </div>
       )
    }
