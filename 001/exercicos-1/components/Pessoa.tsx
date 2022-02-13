34324fb4fcb4809f191c8f4982f9e1d251f1ac9c
codingscode <heronnestg@hotmail.com>
2022-02-12 22:26:30 -0300

    parte 84

---
+++ 001/exercicos-1/components/Pessoa.tsx
@@ -0,0 +1,19 @@
+
+interface PessoaProps {
+   nome: String
+   idade?: number
+}
+
+
+export default function Pessoa(props: PessoaProps) {
+
+   return (
+      <div>
+         <div>Nome : {props.nome}</div>
+         <div>Idade: {props.idade ?? 'Não informada' }</div>
+      </div>
+   )
+}
+
+
+
---
+++ 001/exercicos-1/pages/ts/exemplo.tsx
@@ -0,0 +1,17 @@
+import Pessoa from '../../components/Pessoa'
+
+
+
+export default function exemploTS() {
+
+   return (
+      <div>
+         <Pessoa nome="Aladin" idade={3} />
+         <Pessoa nome="Mel" />
+      </div>
+   )
+}
+
+
+
+
