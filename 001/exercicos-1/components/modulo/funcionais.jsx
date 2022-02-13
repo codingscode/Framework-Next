b712a9fbc2f40362a65bb6672464b68b8baba06d
codingscode <heronnestg@hotmail.com>
2022-02-12 21:44:13 -0300

    parte 80

---
+++ 001/exercicos-1/components/modulo/funcionais.jsx
@@ -0,0 +1,22 @@
+
+
+export function Comp1() {
+
+   return (
+      <h2>Componente 1</h2>
+   )
+}
+
+export const Comp2 = () => {
+
+   return (
+      <h2>Componente 2</h2>
+   )
+}
+
+
+
+
+
+
+
